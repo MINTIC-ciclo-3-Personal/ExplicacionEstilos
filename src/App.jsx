65fa2205e8228d1c2e3a486aec11fd7e2995adf9
[Nikion] <[nigutjim@gmail.com]>
2022-04-12 09:26:41 -0500

    guardar todo en sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import './styles/styles.scss'
 
 function App() {
   return (
     <div className="App">
-      <span>hola mundo</span>
-      <button type="button" className="btn btn-primary">Primary</button>
-      <button type="button" className="btn btn-secondary">Secondary</button>
-      <div className="card">
-        <img src="..." className="card-img-top" alt="..."/>
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
-          </div>
-      </div>
+      <div className='fondo1'>este es el ejemplo1</div>
+      <div className='fondo2'>este es el ejemplo2</div>
+      <div className='fondo3'>este es el ejemplo3</div>
     </div>
   );
 }
