a5cbb4f80f74867d704c7cf6f9db1c695289baa9
[Nikion] <[nigutjim@gmail.com]>
2022-04-12 08:23:09 -0500

    branch del tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-      <span>hola mundo</span>
-      <button type="button" className="btn btn-primary">Primary</button>
-      <button type="button" className="btn btn-secondary">Secondary</button>
-      <div className="card">
-        <img src="..." className="card-img-top" alt="..."/>
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
-          </div>
+      <div>
+        <h1 className="text-3xl font-bold underline">
+          Hello world
+        </h1>
       </div>
     </div>
   );
